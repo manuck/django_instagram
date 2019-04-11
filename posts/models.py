58d4652a4439ddb37230c3308454869218428183
manuck <snc9000@naver.com>
2019-04-11 02:15:15 +0000

    Post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
+class Post(models.Model):
+    content = models.TextField()
+    
+    def __str__(self):
+        return f'Post : {self.pk}'
+    
+    def get_absolute_url(self):
+        return reverse('posts:detail', args=[self.pk])
