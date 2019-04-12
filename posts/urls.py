39cdc1154ad16b0672fbe5d20ee1e27e8f086788
manuck <snc9000@naver.com>
2019-04-12 02:23:13 +0000

    posts|list, detail, new, delete, update

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
-app_name = 'posts'
 
+
+app_name = 'posts'
 urlpatterns = [
     path('', views.list, name='list'),
+    path('new/', views.new, name='new'),
+    path('detail/<int:post_pk>/', views.detail, name='detail'),
+    path('delete/<int:post_pk>/', views.delete, name='delete'),
+    path('update/<int:post_pk>/', views.update, name='update'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def list(request):
-    return render(request, 'posts/list.html')
+    posts = Post.objects.all()
+    return render(request, 'posts/list.html', {'posts': posts})
+    
+def new(request):
+    post = Post()
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save()
+            return redirect('posts:list')
+    else:
+        form = PostForm()
+    context = {'form': form}
+    return render(request, 'posts/forms.html', context)
+    
+def detail(request, post_pk):
+    post = get_object_or_404(Post, pk=post_pk)
+    return render(request, 'posts/detail.html', {'post': post})
+    
+def delete(request, post_pk):
+    post = get_object_or_404(Post, pk=post_pk)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('posts:list')
+    else:
+        return redirect(post)
+        
+def update(request, post_pk):
+    post = get_object_or_404(Post, pk=post_pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save()
+            return redirect(post)
+    else:
+        form = PostForm(instance=post)
+    context = {'form': form}
+    return render(request, 'posts/forms.html', context)
     
